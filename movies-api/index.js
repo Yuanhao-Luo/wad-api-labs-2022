481f69d968ddaa4a0f561d05e7c5d6c57f0d153f
Yuanhao-Luo <1097308076@qq.com>
2022-11-14 12:56:08 +0000

    Week 9 Lab exercise done

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+import genresRouter from './api/genres';
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ const port = process.env.PORT;
 app.use(express.json());
 
 app.use('/api/movies', moviesRouter);
+app.use('/api/genres', genresRouter)
 
 app.listen(port, () => {
     console.info(`Server running at ${port}`);
