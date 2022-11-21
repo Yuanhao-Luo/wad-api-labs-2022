1a8e20cc2fc6d39c78eece63dd9d0338eecefdba
Yuanhao-Luo <1097308076@qq.com>
2022-11-21 12:00:39 +0000

    Mongo lab Users Router

--- movies-api/index.js
+++ movies-api/index.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
 import genresRouter from './api/genres';
+import usersRouter from './api/users';
 import './db';
 import './seedData'
 
@@ -15,6 +16,7 @@ app.use(express.json());
 
 app.use('/api/movies', moviesRouter);
 app.use('/api/genres', genresRouter)
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
     console.info(`Server running at ${port}`);
