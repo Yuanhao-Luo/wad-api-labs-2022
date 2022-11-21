0975e74c968abcb974ddc961debcbdedd51d4185
Yuanhao-Luo <1097308076@qq.com>
2022-11-21 12:06:50 +0000

    Mongo lab User Favourites

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -2,9 +2,15 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
+const MovieSchema = new Schema({
+    id: Number,
+    title: String
+});
+
 const UserSchema = new Schema({
     username: { type: String, unique: true, required: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    favourites: [MovieSchema]
 });
 
 export default mongoose.model('User', UserSchema);
