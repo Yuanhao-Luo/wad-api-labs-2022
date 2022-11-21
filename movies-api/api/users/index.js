f218f02ea1d7080117d0478cbab25f480c05f089
Yuanhao-Luo <1097308076@qq.com>
2022-11-21 12:23:36 +0000

    Mongo lab Error Handling

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import User from './userModel';
+import asyncHandler from 'express-async-handler';
+
 
 const router = express.Router(); // eslint-disable-line
 
@@ -9,23 +11,23 @@ router.get('/', async(req, res) => {
     res.status(200).json(users);
 });
 
-// register(Create)/Authenticate User
-router.post('/', async(req, res) => {
+// register
+router.post('/', asyncHandler(async(req, res) => {
     if (req.query.action === 'register') { //if action is 'register' then save to DB
-        await User(req.body).save();
+        await User(req.body).save()
         res.status(201).json({
             code: 201,
             msg: 'Successful created new user.',
         });
-    } else { //Must be an authenticate then!!! Query the DB and check if there's a match
+    } else { //Must be authenticating the!!! Query the DB and check if there's a match
         const user = await User.findOne(req.body);
         if (!user) {
-            return res.status(401).json({ code: 401, msg: 'Authentication failed' });
+            return res.status(401).json({ code: 401, msg: 'Authentication failed' })
         } else {
-            return res.status(200).json({ code: 200, msg: "Authentication Successful", token: 'TEMPORARY_TOKEN' });
+            return res.status(200).json({ code: 200, msg: "Authentication Successful", token: 'TEMPORARY_TOKEN' })
         }
     }
-});
+}));
 
 // Update a user
 router.put('/:id', async(req, res) => {
