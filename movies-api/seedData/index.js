a3a452b4e1db120f132b0bdf1a44aa57ae917d3d
Yuanhao-Luo <1097308076@qq.com>
2022-12-05 12:25:11 +0000

    Topic 11 - Update Movies Data/Route

--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -2,6 +2,8 @@ import userModel from '../api/users/userModel';
 import genresModel from '../api/genres/genresModel';
 import users from './users';
 import geners from './genres'
+import movieModel from '../api/movies/movieModel';
+import movies from './movies.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -29,7 +31,21 @@ async function loadGenres() {
     }
 }
 
+// deletes all movies documents in collection and inserts test data
+export async function loadMovies() {
+    console.log('load seed data');
+    console.log(movies.length);
+    try {
+        await movieModel.deleteMany();
+        await movieModel.collection.insertMany(movies);
+        console.info(`${movies.length} Movies were successfully stored.`);
+    } catch (err) {
+        console.error(`failed to Load movie Data: ${err}`);
+    }
+}
+
 if (process.env.SEED_DB) {
     loadUsers();
     loadGenres();
+    loadMovies();
 }
