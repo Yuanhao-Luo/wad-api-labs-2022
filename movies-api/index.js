8ba42af79552fce6ec918fd29e098ea97284a98d
Yuanhao-Luo <1097308076@qq.com>
2022-12-05 11:53:11 +0000

    Topic 11 - Session based Auth

--- movies-api/index.js
+++ movies-api/index.js
@@ -3,6 +3,8 @@ import express from 'express';
 import moviesRouter from './api/movies';
 import genresRouter from './api/genres';
 import usersRouter from './api/users';
+import session from 'express-session';
+import authenticate from './authenticate';
 import './db';
 import './seedData'
 
@@ -23,7 +25,13 @@ const port = process.env.PORT;
 
 app.use(express.json());
 
-app.use('/api/movies', moviesRouter);
+app.use(session({
+    secret: 'ilikecake',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use('/api/movies', authenticate, moviesRouter);
 app.use('/api/genres', genresRouter)
 app.use('/api/users', usersRouter);
 app.use(errHandler);
