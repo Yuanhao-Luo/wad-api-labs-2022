1f30e2f4d6f87dfba9935d628fba6f28465039a6
Yuanhao-Luo <1097308076@qq.com>
2022-12-05 12:15:08 +0000

    Topic 11 - Passport

--- movies-api/index.js
+++ movies-api/index.js
@@ -5,6 +5,7 @@ import genresRouter from './api/genres';
 import usersRouter from './api/users';
 import session from 'express-session';
 import authenticate from './authenticate';
+import passport from './authenticate';
 import './db';
 import './seedData'
 
@@ -25,13 +26,9 @@ const port = process.env.PORT;
 
 app.use(express.json());
 
-app.use(session({
-    secret: 'ilikecake',
-    resave: true,
-    saveUninitialized: true
-}));
+app.use(passport.initialize());
 
-app.use('/api/movies', authenticate, moviesRouter);
+app.use('/api/movies', passport.authenticate('jwt', { session: false }), moviesRouter);
 app.use('/api/genres', genresRouter)
 app.use('/api/users', usersRouter);
 app.use(errHandler);
